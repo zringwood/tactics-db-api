089a3a6911fbe1cffafedcf36ed4b1d4f971334c
Zachary Ringwood <zringwood@gmail.com>
2023-07-23 19:46:09 -0400

    Changed database.

--- seeds/tentactics.js
+++
@@ -1,9 +0,0 @@
-const puzzles = require('../data/tentactics.json')
-
-exports.seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('puzzles').delete()
-  await knex("puzzles").insert(puzzles);
-  
-  
-};
