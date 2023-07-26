b78441cac781e427133891a062a9af12cc4e571c
Zachary Ringwood <zringwood@gmail.com>
2023-07-26 15:56:13 -0400

    Added endgame difficulty route.

--- server.js
+++ server.js
@@ -41,7 +41,15 @@ app.get("/endgames/:id", (req, res) => {
     })
 
 })
-
+//Serves a middlegame puzzle with a given difficulty, within 100 rating points either side. 
+app.get("/endgames/difficulty/:rating", (req, res) => {
+    const rating = Number(req.params.rating);
+    knex.raw(` select * from endgames where Rating > ${rating-100} AND Rating < ${rating+100} order by rand() LIMIT 1;`).then(response => {
+        if (response.length > 0)
+            return res.status(200).send(response[0])
+        return res.status(404).send(`No puzzle with id ${req.params.id} found!`)
+    })
+})
 //Serve a puzzle
 app.get("/puzzle/:id", (req, res) => {
     const id = req.params.id;
