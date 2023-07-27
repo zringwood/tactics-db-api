3d657434039897d024994ff6f9801712a684a195
Zachary Ringwood <zringwood@gmail.com>
2023-07-27 11:08:35 -0400

    Split the database up for faster queries.

--- routes/middlegames.js
+++ routes/middlegames.js
@@ -11,14 +11,23 @@ router.get("/:id", (req, res) => {
     }).catch(error => {
         res.status(500).send(error);
     })
-
 })
-//Serves a middlegame puzzle with a given difficulty, within 100 rating points either side. 
-router.get("/difficulty/:rating", (req, res) => {
-    const rating = Number(req.params.rating);
-    knex.raw(` select * from middlegames where Rating > ${rating-100} AND Rating < ${rating+100} order by rand() LIMIT 1;`).then(response => {
+//Returns the number of puzzles in the database with a given difficulty
+router.get("/:difficulty/count", (req, res) => {
+    const difficulty = req.params.difficulty;
+    knex.raw(` select count(*) from middlegames_${difficulty}`).then(response => {
         if (response.length > 0)
-            return res.status(200).send(response[0])
+            return res.status(200).send(response[0][0])
+        return res.status(404).send(`No puzzle with id ${req.params.id} found!`)
+    })
+})
+//Serves a middlegame puzzle with a given difficulty and a given id. 
+router.get("/:difficulty/:id", (req, res) => {
+    const id = req.params.id;
+    const difficulty = req.params.difficulty;
+    knex.raw(` select * from middlegames_${difficulty} where id = ${id}`).then(response => {
+        if (response.length > 0)
+            return res.status(200).send(response[0][0])
         return res.status(404).send(`No puzzle with id ${req.params.id} found!`)
     })
 })
